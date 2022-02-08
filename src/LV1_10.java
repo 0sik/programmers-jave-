639694f4b9d0d3886dd3740ca70383669ee5eb78
0sik <ghdtlrdudtlr@naver.com>
2022-02-09 00:19:34 +0900

    LV1_8,LV1_10 수정

--- src/LV1_10.java
+++ src/LV1_10.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution10 {
     public int[] solution(int[] arr) {
         if(arr.length ==1)
             return new int[]{-1};
@@ -17,4 +17,16 @@ class Solution {
         }
             return answer;
     }
+}
+
+public class LV1_10{
+	public static void main(String[] args) {
+		Solution10 test = new Solution10();
+		int[] arr= {4,5,6,7};
+		int [] answer = test.solution(arr);
+		for(int i=0;i<answer.length;i++) {
+			System.out.println(answer[i]);
+
+	}
+}
 }
--- src/LV1_8.java
+++ src/LV1_8.java
@@ -15,7 +15,10 @@ public class LV1_8 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution8 solution = new Solution8();
-		System.out.println(solution.solution(8,4));
+		long [] answer = solution.solution(4,3);
+		for(int i=0;i<answer.length;i++) {
+			System.out.println(answer[i]);
 	}
 
+}
 }
