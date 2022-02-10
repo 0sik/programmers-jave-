8ea9376c9c5890eba47344f54b1c5b5606d060e8
0sik <ghdtlrdudtlr@naver.com>
2022-02-10 23:22:29 +0900

    LV1_12 수정

--- src/LV1_12.java
+++ src/LV1_12.java
@@ -1,7 +1,7 @@
-//정수 제곱근 판별
+
 import java.util.*;
 
-class Solution {
+class Solution12 {
   public long solution(long n) {
       Double sqrt = Math.sqrt(n);
       
@@ -9,4 +9,13 @@ class Solution {
           return (long)Math.pow(sqrt + 1, 2);
       } else return -1;
   }
+}
+
+public class LV1_12{
+	public static void main(String[] args) {
+		Solution12 solution = new Solution12();
+		System.out.println(solution.solution(121));
+
+	}
+	
 }
