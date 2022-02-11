2b58aaa07c13a1781cf6987fad64bd76fdd59d9e
0sik <ghdtlrdudtlr@naver.com>
2022-02-12 01:04:08 +0900

    LV1_14 적용

--- src/LV1_14.java
+++ src/LV1_14.java
@@ -1,6 +1,5 @@
-//문자열 내림차순으로 배열하기
 import java.util.*;
-class Solution {
+class Solution14 {
     public String solution(String s) {
 		Character [] arr = new Character [s.length()];
 		for(int i=0; i<arr.length; i++) {
@@ -14,4 +13,17 @@ class Solution {
 		return s;
     }
 
+}
+public class LV1_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution14 solution = new Solution14();
+		String answer = "hello";
+		String arr= solution.solution(answer);
+		String[] str = arr.split("");
+		for(int i=0;i<str.length;i++) {
+			System.out.println(str[i]);
+	}
+	}
 }
