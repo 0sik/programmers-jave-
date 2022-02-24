13bafaf61438ef2d3d9bb1a59f6e4f95f7a6f435
0sik <ghdtlrdudtlr@naver.com>
2022-02-24 23:46:27 +0900

    LV1_17

--- src/LV1_17.java
+++ src/LV1_17.java
@@ -1,4 +1,12 @@
-
+class solution{
+	public long [] solution(int x, int n)
+		long[]answer = new long[n];
+		long base = x;
+		for(int i = 0; i < n; i++) {
+			answer[i]=base*(i+1);
+		}
+		return aswer;
+}
 public class LV1_17 {
 
 	public static void main(String[] args) {
